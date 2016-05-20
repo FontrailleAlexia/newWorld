8d12ad55d0e088618a71ffcecdff58cf995cc854
FontrailleAlexia <afontraille@gmail.com>
2016-05-20 09:15:14 +0200

    Add files via upload

---
+++ connexion/connexion.php
@@ -0,0 +1,7 @@
+<?php
+$dbServer='localhost';//le serveur
+$dbUser='root';//l'utilisateur de la base
+$dbPass='';//le mot de passe
+$dbName='newWorld';//le nom de la base phpmyadmin
+$laBase=mysqli_connect($dbServer,$dbUser,$dbPass,$dbName);
+?>
